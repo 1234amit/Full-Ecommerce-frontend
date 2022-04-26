a17993b4b2b824af77b864480fbf667bc504cde6
@1234amit <cseiubatgoswami@gmail.com>
2022-04-27 02:31:47 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import {
 } from "react-router-dom";
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 function App() {
@@ -24,6 +27,9 @@ function App() {
             <Route path="/" component={HomeScreen} exact/>
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
           </Container>
         </main>
       <Footer />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import { logout } from '../../actions/userActions'
 const Header = () => {
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    // logout section
+    const dispatch = useDispatch()
+
+    const logoutHandler = () =>{
+        dispatch(logout())
+    }
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg">
@@ -9,9 +20,20 @@ const Header = () => {
                     <Navbar.Brand as={NavLink} to="/">AmitShop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto">
                         <Nav.Link as={NavLink} to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                        <Nav.Link as={NavLink} to="/login"><i className="fas fa-sign-in-alt"></i> Login</Nav.Link>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id="username">         
+                                <Nav.Link as={NavLink} to="/profile">
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </Nav.Link>
+
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ):(
+                            <Nav.Link as={NavLink} to="/login"><i className="fas fa-sign-in-alt"></i> Login</Nav.Link>
+                        )}
+                        
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
